5da7a125bba54a41baf51bc056d9d3eedfc309a0
marco <service0026@aibooksbank.com>
2024-05-21 17:53:08 +0800

    message

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,16 +4,22 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
 
+  let playerName = <span className="player-name">{name}</span>
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
   }
 
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
